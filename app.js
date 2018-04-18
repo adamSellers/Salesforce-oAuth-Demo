794301a9d432b267432c24fbb61064003a4c5328
Felipe Ryan <fryan@fryan-ltm3.internal.salesforce.com>
2018-04-18 13:05:53 +0200

    endpoint env var and user template for better json output

--- app.js
+++ app.js
@@ -32,12 +32,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  store: new RedisStore({
-    url: process.env.REDIS_URL
-  }),
-  secret: process.env.REDISSECRET,
-  resave: false,
-  saveUninitialized: false
+    store: new RedisStore({
+        url: process.env.REDIS_URL
+    }),
+    secret: process.env.REDISSECRET,
+    resave: false,
+    saveUninitialized: false
 }));
 
 app.use('/', index);
@@ -45,20 +45,20 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -4,89 +4,93 @@ var clientId = process.env.CLIENTID;
 var clientSecret = process.env.CLIENTSECRET;
 var callbackUrl = process.env.CALLBACKURL;
 
-function login (req, res, next) {
-  // we simply just redirect the user to the Salesforce login service.
-  res.redirect(`https://login.salesforce.com/services/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${callbackUrl}&display=popup`);
+var salesforce_endpoint = process.env.SALESFORCE_ENDPOINT;
+
+function login(req, res, next) {
+    // we simply just redirect the user to the Salesforce login service.
+    res.redirect(`${salesforce_endpoint}/services/oauth2/authorize?response_type=code&client_id=${clientId}&redirect_uri=${callbackUrl}&display=popup`);
 };
 
-function callback (req, res, next) {
-  // first we harvest the code variable
-  let authCode = req.query.code;
+function callback(req, res, next) {
+    // first we harvest the code variable
+    let authCode = req.query.code;
 
-  // the URL that we are going to post the auth code to
-  let tokenUrl = 'https://login.salesforce.com/services/oauth2/token';
-  // post to token url to get access token using the request library
-  request.post(tokenUrl, {
-    form: {
-      code: authCode,
-      grant_type: 'authorization_code',
-      client_id: clientId,
-      client_secret: clientSecret,
-      redirect_uri: callbackUrl
-    },
-    json: true
-  }, function (err, data) {
-    // handle errors if present
-    if (err) {
-      return next(err);
-    }
-
-    // set session data in redis
-    req.session.loginCounter++;
-    req.session.authInfo = {
-      accessToken: data.body.access_token,
-      refreshToken: data.body.refresh_token,
-      userId: data.body.id,
-      signature: data.body.signature,
-      instanceUrl: data.body.instance_url,
-      issuedAt: data.body.issued_at,
-      userIsAuthenticated: true
-    }
-    req.session.save();
-    console.log('session data saved as: ' + JSON.stringify(req.session.authInfo));
-    // send user back to homepage to view stats
-    res.redirect('/');
-  });
+    // the URL that we are going to post the auth code to
+    let tokenUrl = `${salesforce_endpoint}/services/oauth2/token`;
+    // post to token url to get access token using the request library
+    request.post(
+        tokenUrl,
+        {
+            form: {
+                code: authCode,
+                grant_type: 'authorization_code',
+                client_id: clientId,
+                client_secret: clientSecret,
+                redirect_uri: callbackUrl
+            },
+            json: true
+        },
+        function (err, data) {
+            // handle errors if present
+            if (err) {
+                return next(err);
+            }
+            // set session data in redis
+            req.session.loginCounter++;
+            req.session.authInfo = {
+                accessToken: data.body.access_token,
+                refreshToken: data.body.refresh_token,
+                userId: data.body.id,
+                signature: data.body.signature,
+                instanceUrl: data.body.instance_url,
+                issuedAt: data.body.issued_at,
+                userIsAuthenticated: true
+            }
+            req.session.save();
+            console.log('session data saved as: ' + JSON.stringify(req.session.authInfo));
+            // send user back to homepage to view stats
+            res.redirect('/');
+        });
 };
 
 // logout function that revokes the oAuth token from Salesforce
-function logout (req, res, next) {
-  // first define the URL for the token revoke service
-  let revokeUrl = 'https://login.salesforce.com/services/oauth2/revoke';
+function logout(req, res, next) {
+    // first define the URL for the token revoke service
+    let revokeUrl = `${salesforce_endpoint}/services/oauth2/revoke`;
 
-  // POST request with data
-  request.post(revokeUrl, {
-    form: {
-      token: req.session.authInfo.accessToken
-    }
-  }, function (err, data) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log('everything is invalidated: ' + JSON.stringify(data));
-    // destroy the session and redirect to root
-    req.session.destroy();
-    res.redirect('/');
-  });
+    // POST request with data
+    request.post(revokeUrl, {
+        form: {
+            token: req.session.authInfo.accessToken
+        }
+    }, function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('everything is invalidated: ' + JSON.stringify(data));
+        // destroy the session and redirect to root
+        req.session.destroy();
+        res.redirect('/');
+    });
 }
 
 // function to check if logged in
-function isLoggedIn (err, req, res, next) {
-  if (err) {
-    return next(err);
-  }
-  if (!req.session.authInfo.userIsAuthenticated) {
-    console.log('user must login first!');
-    res.redirect('/');
-  } else {
-    next();
-  }
+function isLoggedIn(err, req, res, next) {
+    if (err) {
+        return next(err);
+    }
+    if (!req.session.authInfo.userIsAuthenticated) {
+        console.log('user must login first!');
+        res.redirect('/');
+    } else {
+        next();
+    }
 }
 
 // Export the functions to be used in the router.
 module.exports = {
-  login: login,
-  callback: callback,
-  logout: logout,
-  isLoggedIn: isLoggedIn
+    login: login,
+    callback: callback,
+    logout: logout,
+    isLoggedIn: isLoggedIn
 }
